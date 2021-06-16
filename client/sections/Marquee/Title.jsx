5ebfdbd28646e64064c8f801ccd2a6a8bafbd286
Melissa Ganzfried <melissa.l.ganzfried@biola.edu>
2021-06-16 09:58:38 +0200

    update photo-grid.css

--- client/sections/Marquee/Title.jsx
+++ client/sections/Marquee/Title.jsx
@@ -4,11 +4,10 @@ import StarIcon from '@material-ui/icons/Star';
 
 const Title = ({ listing }) => {
   const { title, rating, location } = listing;
-  if (!listing) { // skeletons
+  if (!listing) {
     return ( <Skeleton variant="text" /> );
-  } // should have a record available
+  }
   return (
-  // use react fragment here?
     <div>
       <h2>{title}</h2>
       <h3>
