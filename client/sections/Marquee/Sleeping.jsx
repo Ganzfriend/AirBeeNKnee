24c51edeb8ee6912cd6d2471c4074c2156a8fb30
Melissa Ganzfried <melissa.l.ganzfried@biola.edu>
2021-06-16 09:54:18 +0200

    update Sleeping

--- client/sections/Marquee/Sleeping.jsx
+++ client/sections/Marquee/Sleeping.jsx
@@ -6,19 +6,19 @@ const Sleeping = ({ listing }) => {
   if (!listing) {
     return null;
   }
-  let roomNum = 1;
+  var roomNum = 1;
   return (
     <div>
       <h2>Sleeping arrangements</h2>
-      {bedrooms?.map((bedroom) => (
-        <div className="marquee-sleeping-span">
-          <h3>
-            Bedroom
-            {roomNum++}
-          </h3>
-          <h6>{bedroom}</h6>
-        </div>
-      ))}
+      <div className="marquee-sleeping-flexbox">
+        {bedrooms?.map((bedroom) => (
+          <div className="marquee-sleeping-card">
+            <h2><KingBedIcon /></h2>
+            <h4>Bedroom {roomNum++}</h4>
+            <h6>{bedroom}</h6>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
