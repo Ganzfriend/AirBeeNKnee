b45e3fc3873b460e22343838ea82418cf2e37797
Melissa Ganzfried <melissa.l.ganzfried@biola.edu>
2021-06-18 09:44:08 +0200

    update Reviews Review.js to JSS

--- client/sections/Reviews/components/Review.js
+++ client/sections/Reviews/components/Review.js
@@ -3,9 +3,16 @@ import axios from 'axios';
 import ReviewModal from './ReviewModal.jsx';
 import { Card } from 'react-bootstrap';
 import Moment from 'moment';
+import {makeStyles} from '@material-ui/core/styles';
+
+import styles from '../../../styles.js';
+
+const useStyles = makeStyles(styles);
 
 
 const Review = ({ reviews, id = 1 }) => {
+  const classes = useStyles();
+
   const [modal, setModal] = useState(false);
 
   const [ratings, setRatings] = useState({});
@@ -18,18 +25,18 @@ const Review = ({ reviews, id = 1 }) => {
 
   return reviews !== [] ? (
     <div>
-      <div id='reviews-container'>
+      <div className={classes.reviewsContainer}>
         <div>
           {reviews?.slice(0, 2).map(({ review }) => (
             <div key={review?.name}>
-              <div className='reviewer-title-bar'>
+              <div className={classes.reviewerTitleBar}>
                 <img src='https://source.unsplash.com/random/100x100/?person' alt='' />
-                <div id='reviewer-title-info'>
-                  <div id='review-title-name' >{review.name.split(' ')[0]}</div>
-                  <div id='review-title-joined' >{Moment(new Date(review.created_at)).format('MMMM YYYY')}</div>
+                <div className={classes.reviewerTitleInfo}>
+                  <div className={classes.reviewTitleName} >{review.name.split(' ')[0]}</div>
+                  <div className={classes.reviewTitleJoined} >{Moment(new Date(review.created_at)).format('MMMM YYYY')}</div>
                 </div>
               </div>
-              <div id='review-body' >{review.body}</div>
+              <div className={classes.reviewBody} >{review.body}</div>
               <br />
               <br />
             </div>
@@ -38,20 +45,20 @@ const Review = ({ reviews, id = 1 }) => {
         <div>
           {reviews.slice(2, 4).map(({ review }) => (
             <div key={review.name}>
-              <div className='reviewer-title-bar'>
+              <div className={classes.reviewerTitleBar}>
                 <img src='https://source.unsplash.com/random/100x100/?person' alt='' />
-                <div id='reviewer-title-info'>
-                  <div id='review-title-name' >{review.name.split(' ')[0]}</div>
-                  <div id='review-title-joined' >{Moment(new Date(review.created_at)).format('MMMM YYYY')}</div>
+                <div className={classes.reviewerTitleInfo}>
+                  <div className={classes.reviewTitleName} >{review.name.split(' ')[0]}</div>
+                  <div className={classes.reviewTitleJoined} >{Moment(new Date(review.created_at)).format('MMMM YYYY')}</div>
                 </div>
               </div>
-              <div id='review-body' >{review.body}</div>
+              <div className={classes.reviewBody} >{review.body}</div>
               <br />
             </div>
           ))}
         </div>
       </div>
-      <button id='show-revs-btn' onClick={() => setModal(true)}>{`Show all ${reviews.length} reviews`}</button>
+      <button id='show-revs-btn' className={classes.showRevsBtn} onClick={() => setModal(true)}>{`Show all ${reviews.length} reviews`}</button>
       <br />
       <br />
       <ReviewModal ratings={ratings} reviews={reviews} show={modal} setModal={setModal} />
